6fa31cd520f8ef4a1973f31e26472ec730c644a6
soniag0125 <soniag0125@hotmail.com>
2024-07-19 16:35:13 -0500

    Initial commit

---
+++ src/main/java/com/sguama/book_trips/book_trips/BookTripsApplication.java
@@ -0,0 +1,13 @@
+package com.sguama.book_trips.book_trips;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookTripsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookTripsApplication.class, args);
+	}
+
+}
